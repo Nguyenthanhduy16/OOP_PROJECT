4a72133f37440fe10b6606733b4f1ccd2eb7c5a4
Trinh Yen Nhi <trinhyennhi0705@gmail.com>
2025-05-11 17:05:16 +0700

    Activity_User

--- ICTSV/src/Activity.java
+++ ICTSV/src/Activity.java
@@ -1,6 +1,37 @@
-
 public class Activity {
+    private String title;
+    private String name;
+    private boolean status;
+    private int score;
 
-	
+    public Activity(String title, String name, boolean status, int score) {
+        this.title = title;
+        this.name = name;
+        this.status = status;
+        this.score = score;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public boolean isStatus() {
+        return status;
+    }
+
+    public int getScore() {
+        return score;
+    }
+
+    public void display() {
+        System.out.println(this.getTitle() + "\n"
+                + this.getName() + "\n"
+                + this.isStatus() + "\n"
+                + "Điểm: " + this.getScore());
+    }
 
 }
