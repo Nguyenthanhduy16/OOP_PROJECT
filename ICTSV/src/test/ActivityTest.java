86c76c657b3c9e829a266b73ff6d5ff475340764
Trinh Yen Nhi <trinhyennhi0705@gmail.com>
2025-06-04 17:14:31 +0700

    Test thao tac voi json Admin

--- ICTSV/src/test/ActivityTest.java
+++ ICTSV/src/test/ActivityTest.java
@@ -1,25 +1,47 @@
 package test;
 
 import java.io.File;
+import java.util.List;
+import java.util.Scanner;
+
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 import model.Activity;
 import model.Admin;
+import model.User;
 
 public class ActivityTest {
     public static void main(String[] args) {
-        // Tạo hoạt động mới
-        Activity newActivity = new Activity("HocTap", "Quản lý công việc", true, 15, "29/05/2025", "Bách khoa");
+        try {
+            ObjectMapper mapper = new ObjectMapper();
 
-        // Tạo tài khoản admin
-        Admin admin = new Admin("admin001", "admin", "admin123");
+            // Đọc danh sách user từ file JSON
+            List<User> users = mapper.readValue(new File("src/data/data.json"),
+                    new TypeReference<List<User>>() {});
 
-        // Thêm hoạt động và lưu vào file JSON
-       admin.addNewActivity(newActivity);
+            // Kiểm tra nếu là Admin thì cho phép thêm và xóa hoạt động
+            for (User user : users) {
+                if (user instanceof Admin) {
+                    Admin admin = (Admin) user;
+                    System.out.println("\nĐăng nhập thành công với Admin: " + admin.getUserName());
 
-        // In ra tất cả các hoạt động
-        admin.printActivity();
-        
-       //admin.removeOutdatedActivity(newActivity);
+                    // Ví dụ thêm hoạt động
+                    Activity newActivity = new Activity("HocTap", "Quản lý hệ thống", false, 20, "29/05/2025", "Nhà C2 Đại học Bách khoa Hà Nội");
+                    
+                    //Thêm hoạt động
+                    admin.addNewActivity(newActivity);
+                    
+                    //Xóa hoạt động
+                    //admin.removeOutdatedActivity(newActivity);
+                    
+                    admin.printActivity();
+                    
+                }
+            }
 
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
